75bb4c562b5b9c240d45316b8047a39a0c3779b4
Amol799 <166220003+Amol799@users.noreply.github.com>
2024-08-31 17:36:12 +0530

    first email server checkin

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+const details = require("./details.json");
+
+const app = express();
+app.use(cors({ origin: "*" }));
+app.use(bodyParser.json());
+
+app.listen(8081, () => {
+  console.log("The server started on port 8081 !!!!!!");
+});
+
+app.get("/", (req, res) => {
+  res.send(
+    "<h1 style='text-align: center'>Wellcome to FunOfHeuristic <br><br>😃👻😃👻😃👻😃👻😃</h1>"
+  );
+});
+
+app.post("/sendmail", (req, res) => {
+  console.log("request came");
+  let user = req.body;
+  sendMail(user, info => {
+    console.log(`The mail has beed send 😃 and the id is ${info.messageId}`);
+    res.send(info);
+  });
+});
+
+async function sendMail(user, callback) {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({   
+    service:"gmail", 
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    logger:true,
+    debug:true,
+    secureConnection: false,
+    auth: {
+      user: details.email,
+      pass: details.password
+    },
+    tls:{
+      rejectUnauthorized :true
+    }
+  });
+
+  let mailOptions = {
+    from: 'This is from Akshaj Cold Press Oil', // sender address
+    to: user.email, // list of receivers
+    subject: "Akshaj Cold Press Oil Enquiry for : " + user.name, // Subject line
+    html: `<h1>Hi ${user.name}</h1><br>
+    <h4>Thanks for joining us</h4>`
+  };
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail(mailOptions);
+
+  callback(info);
+}
+
+// main().catch(console.error);
